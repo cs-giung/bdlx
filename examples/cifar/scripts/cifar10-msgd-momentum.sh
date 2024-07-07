16bc3a1cb6c7ba8256c27a2b8ccb28e5b7c940a0
cs-giung <giung@kaist.ac.kr>
2024-07-07 13:32:36 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar10-msgd-momentum.sh
@@ -0,0 +1,10 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_msgd.py \
+    --data_name cifar10 \
+    --optim_lr 0.03 \
+    --optim_lr_min 0.0 \
+    --optim_l2 0.003 \
+    --momentum 0.9 \
+    --nesterov false \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10/msgd-momentum/$SEED ; done
