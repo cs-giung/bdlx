900dea84ef48595e2c0f3632c5e9a72e399a9511
cs-giung <giung@kaist.ac.kr>
2024-07-07 18:42:06 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar10-simple-msgd-nesterov.sh
@@ -0,0 +1,11 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_msgd.py \
+    --data_name cifar10 \
+    --data_augmentation simple \
+    --optim_lr 0.03 \
+    --optim_lr_min 0.0 \
+    --optim_l2 0.001 \
+    --momentum 0.9 \
+    --nesterov true \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10-simple/msgd-nesterov/$SEED ; done
