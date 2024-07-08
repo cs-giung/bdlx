c809d0c2bd4a9d4935cb06adffaf7268a81d692d
cs-giung <giung@kaist.ac.kr>
2024-07-08 16:13:49 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar10/cifar10-sgld-cyclical.sh
@@ -0,0 +1,9 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_sgld.py \
+    --data_name cifar10 \
+    --step_size 0.00001 \
+    --step_size_min 0.0 \
+    --posterior_temperature 1.0 \
+    --prior_variance 0.2 \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10/sgld-cyclical/$SEED ; done
