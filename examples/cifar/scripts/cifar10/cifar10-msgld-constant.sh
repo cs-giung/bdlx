e6f799358bce21c637f446b71e144a4f0c259720
cs-giung <giung@kaist.ac.kr>
2024-07-09 08:40:00 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar10/cifar10-msgld-constant.sh
@@ -0,0 +1,10 @@
+for SEED in 8 9 ; \
+  do python examples/cifar/run_msgld.py \
+    --data_name cifar10 \
+    --step_size 0.000003 \
+    --posterior_temperature 1.0 \
+    --prior_variance 0.1 \
+    --smoothing 0.9 \
+    --bias 1.0 \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10/msgld-constant/$SEED ; done
---
+++ examples/cifar/scripts/cifar10/cifar10-msgld-cyclical.sh
@@ -0,0 +1,11 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_msgld.py \
+    --data_name cifar10 \
+    --step_size 0.000003 \
+    --step_size_min 0.0 \
+    --posterior_temperature 1.0 \
+    --prior_variance 0.1 \
+    --smoothing 0.9 \
+    --bias 1.0 \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10/msgld-cyclical/$SEED ; done
