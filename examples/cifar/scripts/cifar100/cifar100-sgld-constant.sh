79d02b79818f3f77d33d17b4161547e5e5b1aecc
cs-giung <giung@kaist.ac.kr>
2024-07-12 16:33:56 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar100/cifar100-sgld-constant.sh
@@ -0,0 +1,8 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_sgld.py \
+    --data_name cifar100 \
+    --step_size 0.00001 \
+    --posterior_temperature 1.0 \
+    --prior_variance 0.2 \
+    --seed $SEED \
+    --save examples/cifar/save/cifar100/sgld-constant/$SEED ; done
