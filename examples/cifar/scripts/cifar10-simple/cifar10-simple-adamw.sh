946d6a95030cd0aacf236f1c131db813c1154a34
cs-giung <giung@kaist.ac.kr>
2024-07-26 14:23:09 +0900

    feat: upload examples/cifar/scripts

---
+++ examples/cifar/scripts/cifar10-simple/cifar10-simple-adamw.sh
@@ -0,0 +1,11 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_adam.py \
+    --data_name cifar10 \
+    --data_augmentation simple \
+    --optim_lr 0.001 \
+    --optim_lr_min 0.0 \
+    --optim_wd 0.0003 \
+    --momentum_mu 0.9 \
+    --momentum_nu 0.99999 \
+    --seed $SEED \
+    --save examples/cifar/save/cifar10-simple/adamw/$SEED ; done
