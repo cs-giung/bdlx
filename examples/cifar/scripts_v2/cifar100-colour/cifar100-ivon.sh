26015666c724e97d15a154a1d7860548e55b4708
cs-giung <giung@kaist.ac.kr>
2024-10-26 01:01:56 +0900

    docs: update examples/cifar/scripts_v2

---
+++ examples/cifar/scripts_v2/cifar100-colour/cifar100-ivon.sh
@@ -0,0 +1,13 @@
+for SEED in 0 1 2 3 4 5 6 7 8 9 ; \
+  do python examples/cifar/run_ivon.py \
+    --data_name cifar100 \
+    --data_augmentation colour \
+    --optim_lr 0.3 \
+    --optim_lr_min 0.0 \
+    --optim_l2 0.0001 \
+    --momentum_mu 0.9 \
+    --momentum_nu 0.9999 \
+    --ess_factor 1.0 \
+    --hess_init 1.0 \
+    --seed $SEED \
+    --save examples/cifar/save_v2/cifar100-colour/ivon/$SEED ; done
